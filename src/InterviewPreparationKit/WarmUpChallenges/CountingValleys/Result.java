f5ef07ef8339af10647e2502659dd8ccf9039a5f
Guilherme Alvino <guilhermealvi9@gmail.com>
2025-01-04 18:37:44 -0300

    Resolucao Counting Valleys

---
+++ src/InterviewPreparationKit/WarmUpChallenges/CountingValleys/Result.java
@@ -0,0 +1,57 @@
+package InterviewPreparationKit.WarmUpChallenges.CountingValleys;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int altitude = 0;
+        int valleys = 0;
+
+        for(char step : path.toCharArray()){
+            if(step == 'U'){
+                altitude++;
+            } else if(step == 'D'){
+                altitude--;
+            }
+
+            if(altitude == 0 && step == 'U'){
+                valleys++;
+            }
+        }
+        return valleys;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
